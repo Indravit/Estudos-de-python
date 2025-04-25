0fe83e50b4ba7eedd4be38b58d8dd26d050754f8
Vitor <joaovitorbezerra372@gmail.com>
2025-04-25 16:01:48 -0300

    Add files via upload

---
+++ full.py
@@ -0,0 +1,13 @@
+firts_name = "ada"
+last_name = "lovelace"
+full_name = f" {firts_name} {last_name}"
+print(full_name)
+print(full_name.title())
+print(full_name.upper())
+print(full_name.lower()) 
+print(full_name.strip()) # remove whitespace from both sides
+print (f" Hello, {firts_name} {last_name}!") # f-string
+print (f" Hello, {firts_name} {last_name}!")
+message = f" Hello,{full_name}!"
+print (message)   
+
